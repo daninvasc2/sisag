f57dd8c4086cb2a31e24d10646218af0f496645a
Daniel Nogueira <daninvasc2@gmail.com>
2023-03-13 23:02:29 -0300

    salvando alteracoes. pendente criar tela de cadastro a parte e desfazer tentativa de usar as duas telas em uma so

--- config/DbConfig.php
+++ config/DbConfig.php
@@ -2,8 +2,8 @@
 	class DbConfig {
 			static $SGDB     = "mysql";
 			static $HOST     = "localhost";
-			static $USER     = "usercadprod";
-			static $PASSWORD = "#user-cad-prod";
-			static $DB       = "cadprod";
+			static $USER     = "root";
+			static $PASSWORD = "root";
+			static $DB       = "sisag";
 	}
 ?>
--- view/header.php
+++ view/header.php
@@ -1,4 +1,10 @@
 <div class="container-fluid d-flex justify-content-center bg-secondary position-relative">
     <h2 class="text-white">Sistema de Agenda de Contatos</h2>
-    <button type="button" onclick="sairDoSistema()" class="btn btn-danger center-absolute">Sair do sistema</button>
+    <?php
+        if (isset($_SESSION['usuario'])) {
+            echo '<div class="position-absolute top-0 end-0">
+                    <button type="button" onclick="sairDoSistema()" class="btn btn-danger btn-sair-sistema">Sair do sistema</button>
+                </div>';
+        }
+    ?>
 </div>
